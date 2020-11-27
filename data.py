89164331a8de97105ed67779a4fbb51592e9419b
king398 <mithilsalunkhe@outlook.com>
2020-11-27 20:38:12 +0530

    commit

--- data.py
+++ data.py
@@ -14,19 +14,20 @@ def make_data(image_path, csv_path):
 		csv_path ([path]): [path for the csv image]
 	"""
 	labels_file = pd.read_csv(csv_path)
+	print(labels_file.head())
 	temp1labels = {}
-	labels= {}
-	for i in range(len(labels_file)):
+	labels = {}
+	for i in range(21397):
 		image_name = labels_file.iloc[i, 0]
 		image_label = labels_file.iloc[i, 1]
 		temp_labels = {image_name, image_label}
-		labels.update([temp_labels])
+		temp1labels.update([temp_labels])
 	for i in tqdm(os.listdir(image_path)):
 		path = str(os.path.join(image_path, i))
-		for ix in temp1labels:
+		for ix,i in temp1labels:
 			if ix == i:
-
-
+				print(i)
+				print()
 
 
 make_data(r"F:\Pycharm_projects\Kaggle Cassava\data\train_images",
