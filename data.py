e2e166572f15e2cbba19c1c5f7ed252b8e62015d
king398 <mithilsalunkhe@outlook.com>
2020-11-27 19:33:28 +0530

    commit

--- data.py
+++ data.py
@@ -1,3 +1,14 @@
 import tensorflow as tf
 import os
 import cv2
+import numpy as np
+from tqdm import tqdm
+
+
+def make_data(image_path):
+	for i in tqdm(os.listdir(image_path)):
+		path = os.path.join(image_path, i)
+		print(path)
+
+
+make_data(r"F:\Pycharm_projects\Kaggle Cassava\data\train_images\''")
