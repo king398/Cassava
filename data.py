ec5a648aba68928ed92cc43ecced554b15b22824
king398 <mithilsalunkhe@outlook.com>
2020-11-27 19:40:22 +0530

    commit

--- data.py
+++ data.py
@@ -6,9 +6,12 @@ from tqdm import tqdm
 
 
 def make_data(image_path):
+	
 	for i in tqdm(os.listdir(image_path)):
-		path = os.path.join(image_path, i)
-		print(path)
+		path = str(os.path.join(image_path, i))
+		new_path = str(i)
+		new_path = new_path.replace('.jpg', '')
+		print(new_path)
 
 
-make_data(r"F:\Pycharm_projects\Kaggle Cassava\data\train_images\''")
+make_data(r"F:\Pycharm_projects\Kaggle Cassava\data\train_images")
