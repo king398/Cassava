d620bfd301a1d35458527797e6b5a9cd3be9e45b
king398 <mithilsalunkhe@outlook.com>
2020-11-27 19:42:21 +0530

    commit

--- data.py
+++ data.py
@@ -2,16 +2,22 @@ import tensorflow as tf
 import os
 import cv2
 import numpy as np
+import pandas as pd
 from tqdm import tqdm
 
 
-def make_data(image_path):
-	
+def make_data(image_path, csv_path):
+	"""Make all the images in the given directory
+
+	Args:
+		image_path ([type]): [description]
+	"""
 	for i in tqdm(os.listdir(image_path)):
 		path = str(os.path.join(image_path, i))
 		new_path = str(i)
 		new_path = new_path.replace('.jpg', '')
 		print(new_path)
+	pd.read_csv()
 
 
-make_data(r"F:\Pycharm_projects\Kaggle Cassava\data\train_images")
+make_data(r"F:\Pycharm_projects\Kaggle Cassava\data\train_images",)
