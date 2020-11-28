cf446ee9ee196b4dfc4cfdf54b890757720ccf1a
king398 <mithilsalunkhe@outlook.com>
2020-11-28 21:30:33 +0530

    got it working

--- data.py
+++ data.py
@@ -16,7 +16,8 @@ def make_data(training_folder, labels_file):
 	samples_df["label"] = samples_df["label"].astype("str")
 	samples_df.head()
 	temp_labels = {}
-	image = np.array([]).astype(np.float32)
+
+	image = []
 	lab = []
 	for i in range(len(samples_df)):
 		image_name = samples_df.iloc[i, 0]
@@ -28,13 +29,12 @@ def make_data(training_folder, labels_file):
 		path = os.path.join(training_folder, im)
 		label = temp_labels.get(im)
 		img = cv2.imread(path)
-		img = tf.image.random_crop(img, size=(100, 100, 3))
-		image = np.append(image, img)
+		img = tf.image.random_crop(img, size=(250, 250, 3))
+		image.append(img)
 		lab.append(label)
-
 	labels = np.array(lab).astype(np.float32)
-	np.savez_compressed('labels.npz', data=labels)
-	np.savez_compressed('img.npz', data=image)
+	train = tf.data.Dataset.from_tensor_slices((image, labels))
+	return train
 
 
-
+data = make_data(training_folder=r'data/train_images', labels_file=r'data/train.csv')
