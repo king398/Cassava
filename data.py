dec5f3dea6d7b67eef64b33cd82a453b7e282fb3
king398 <mithilsalunkhe@outlook.com>
2020-11-28 12:03:18 +0530

    got it working

--- data.py
+++ data.py
@@ -1,44 +1,48 @@
-import tensorflow as tf
-import os
-import cv2
 import numpy as np
 import pandas as pd
+import os
 from tqdm import tqdm
+from sklearn.utils import shuffle
+from sklearn.utils import class_weight
+from sklearn.preprocessing import minmax_scale
+import random
+import cv2
+import warnings
 
+warnings.filterwarnings('ignore')
 
-def Make_data(image_path, csv_path):
-	labels_file = pd.read_csv(csv_path)
-	print(labels_file.head())
-	temp1labels = {}
-	labels_list = []
-	list_img = []
-	image_total = 0
-	brr = 0
-	for i in range(21397):
-		image_name = labels_file.iloc[i, 0]
-		image_label = labels_file.iloc[i, 1]
-		brr += 1
-		temp_labels = {image_name, image_label}
-		temp1labels.update([temp_labels])
-	print(temp1labels)
-	for i in tqdm(os.listdir(image_path)):
-		real = temp1labels.get(i)
-		label = real
-		labels_list.append(label)
-		path = str(os.path.join(image_path, i))
-		img = cv2.imread(path)
-		img = cv2.resize(img, dsize=(128, 128))
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers
+from tensorflow.keras import Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Flatten, Dropout, Activation
+from tensorflow.keras.layers import BatchNormalization, GlobalAveragePooling2D
+from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.layers import Dense, Dropout, Activation, Input, BatchNormalization, GlobalAveragePooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import EfficientNetB3
+from tensorflow.keras.layers.experimental.preprocessing import RandomCrop, CenterCrop, RandomRotation
 
-		image_total += 1
-		list_img.append(img)
-		print(real)
-
-	labels = np.asarray(labels_list).astype(np.float32)
-	images = np.asarray(list_img).astype(np.float32)
-	train = tf.data.Dataset.from_tensor_slices((images, labels))
-	return train
-
-
-train = Make_data(image_path=r"../input/cassava-leaf-disease-classification/train_images",
-                  csv_path=r"../input/cassava-leaf-disease-classification/train.csv")
-print(tf.data.experimental.cardinality(train))
+training_folder = r"F:\Pycharm_projects\Kaggle Cassava\data\train_images"
+samples_df = pd.read_csv(r"F:\Pycharm_projects\Kaggle Cassava\data\train.csv")
+samples_df = shuffle(samples_df, random_state=42)
+samples_df["label"] = samples_df["label"].astype("str")
+samples_df.head()
+temp_labels = {}
+for i in range(len(samples_df)):
+	image_name = samples_df.iloc[i, 0]
+	image_label = samples_df.iloc[i, 1]
+	la = {image_name: image_label}
+	temp_labels.update(la)
+print(len(temp_labels))
+for im in tqdm(os.listdir(training_folder)):
+	path = os.path.join(training_folder, im)
+	label = temp_labels.get(im)
+	img = cv2.imread(path)
+	img = cv2.resize(img,d)
+	cv2.imshow("img", img)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+	break
