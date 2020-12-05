7c39d31b6bffa5f202df6e463e18010b47c72214
king398 <mithilsalunkhe@outlook.com>
2020-12-05 13:07:53 +0530

    got it working

--- notebooks/data.py
+++ notebooks/data.py
@@ -9,7 +9,8 @@ from PIL import Image
 from random import randrange
 from tensorflow.keras.models import Sequential
 from tensorflow.keras import layers
-from tensorflow.keras.layers import Dense, Flatten, Dropout, Activation, Conv2D, MaxPool2D, Conv2DTranspose, LeakyReLU
+from tensorflow.keras.layers import Dense, Flatten, Dropout, Activation, Conv2D, MaxPool2D, Conv2DTranspose, LeakyReLU, \
+	BatchNormalization
 import cv2
 
 warnings.filterwarnings("ignore")
@@ -24,47 +25,114 @@ image = []
 lab = []
 
 for i in range(len(samples_df)):
-    image_name = samples_df.iloc[i, 0]
-    image_label = samples_df.iloc[i, 1]
-    la = {image_name: image_label}
-    temp_labels.update(la)
+	image_name = samples_df.iloc[i, 0]
+	image_label = samples_df.iloc[i, 1]
+	la = {image_name: image_label}
+	temp_labels.update(la)
 print(len(temp_labels))
 for im in tqdm(os.listdir(training_folder)):
-    path = os.path.join(training_folder, im)
-    label = temp_labels.get(im)
-    img = Image.open(path)
-    img = img.crop((300, 300, 300, 300))
-    img = tf.keras.preprocessing.image.img_to_array(img, dtype=np.float32)
+	path = os.path.join(training_folder, im)
+	label = temp_labels.get(im)
+	img = cv2.imread(path)
+	imgg = cv2.resize(img, dsize=(300, 300))
+	img = tf.keras.preprocessing.image.img_to_array(imgg, dtype=np.float32)
 
-    img = np.asarray(img).astype(np.float32)
+	img = np.asarray(imgg).astype(np.float32)
 
-    image.append(img)
-    lab.append(label)
+	image.append(imgg)
+	lab.append(label)
 
 print("ok")
+images = np.array(image)
+labels = np.array(lab).astype(np.float32)
+from tensorflow.keras.layers import Dense, Flatten, Dropout, Activation, Conv2D, MaxPool2D, Conv2DTranspose, LeakyReLU
 
-images = tf.convert_to_tensor(image, dtype=tf.float16)
-labels = tf.keras.utils.to_categorical(np.array(lab).astype(np.float32))
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.mixed_precision import experimental as mixed_precision
 
 model = Sequential()
-VGG = tf.keras.applications.VGG19(input_shape=(
-    300, 300, 3), include_top=False, weights=None)
-Resnet = tf.keras.applications.ResNet152(input_shape=(
-    300, 300, 3), include_top=False, weights=None, classes=5)
-Efficient_net = tf.keras.applications.EfficientNetB4(
-    input_shape=(300, 300, 3), include_top=False)
+VGG = tf.keras.applications.VGG19(input_shape=(300, 300, 3), include_top=False, weights=None)
+Resnet = tf.keras.applications.ResNet152(input_shape=(300, 300, 3), include_top=False, weights=None, classes=5)
+Efficient_net = tf.keras.applications.EfficientNetB4(input_shape=(300, 300, 3), include_top=False)
 
 model.add(layers.experimental.preprocessing.Rescaling(1 / 255))
 
 model.add(Resnet)
 model.add(LeakyReLU())
+model.add(BatchNormalization())
+model.add(Conv2DTranspose(filters=512, strides=1, kernel_size=2))
+model.add(BatchNormalization())
+
+model.add(Conv2DTranspose(filters=512, strides=1, kernel_size=2))
+model.add(BatchNormalization())
+
+model.add(Conv2D(filters=512, kernel_size=2, strides=1, activation="relu"))
+model.add(BatchNormalization())
+
+model.add(Conv2D(filters=512, kernel_size=2, strides=1, activation="relu"))
+model.add(BatchNormalization())
+
+model.add(MaxPool2D(pool_size=2, strides=1))
+
+model.add(BatchNormalization())
+model.add(Conv2DTranspose(filters=256, strides=1, kernel_size=2))
+model.add(BatchNormalization())
+
+model.add(Conv2D(filters=256, kernel_size=1, strides=1, activation="relu"))
+model.add(BatchNormalization())
+
+model.add(Conv2D(filters=256, kernel_size=1, strides=1, activation="relu"))
+model.add(BatchNormalization())
+
+model.add(MaxPool2D(pool_size=2, strides=1))
+model.add(BatchNormalization())
+
+model.add(Conv2DTranspose(filters=128, strides=1, kernel_size=2))
+model.add(BatchNormalization())
+
+model.add(Conv2D(filters=128, kernel_size=2, strides=1, activation="relu"))
+model.add(BatchNormalization())
+
+model.add(Conv2D(filters=128, kernel_size=2, strides=1, activation="relu"))
+model.add(BatchNormalization())
+
+model.add(MaxPool2D(pool_size=2, strides=1))
+model.add(BatchNormalization())
+model.add(Conv2D(filters=64, kernel_size=2, strides=1, activation="relu"))
+model.add(BatchNormalization())
+
+model.add(Conv2D(filters=64, kernel_size=2, strides=1, activation="relu"))
+model.add(BatchNormalization())
+
+model.add(MaxPool2D(pool_size=2, strides=1))
+model.add(BatchNormalization())
+
+model.add(Conv2DTranspose(filters=32, strides=1, kernel_size=2))
+model.add(BatchNormalization())
+
+model.add(Conv2D(filters=32, kernel_size=2, strides=1, activation="relu"))
+model.add(BatchNormalization())
+
+model.add(Conv2D(filters=32, kernel_size=2, strides=1, activation="relu"))
+model.add(BatchNormalization())
+
+model.add(MaxPool2D(pool_size=2, strides=1))
+model.add(BatchNormalization())
+model.add(Conv2D(filters=16, kernel_size=2, strides=1, activation="relu"))
+model.add(BatchNormalization())
+
+model.add(Conv2D(filters=16, kernel_size=2, strides=1, activation="relu"))
+model.add(BatchNormalization())
+
+model.add(MaxPool2D(pool_size=2, strides=1))
+model.add(BatchNormalization())
 
 model.add(Flatten())
 model.add(LeakyReLU())
 
 model.add(Dense(256, activation="relu"))
 model.add(LeakyReLU())
-
 model.add(tf.keras.layers.Activation('relu'))
 model.add(LeakyReLU())
 
@@ -86,15 +154,15 @@ model.add(Dense(8, activation="relu"))
 
 model.add(Dense(5, activation="softmax"))
 opt = tf.keras.optimizers.Adagrad(
-    learning_rate=0.01)
+	learning_rate=0.01)
 model.compile(optimizer=opt,
-              loss="categorical_crossentropy",
+              loss="sparse_categorical_crossentropy"
+              ,
               metrics=['accuracy'])
 checkpoint_filepath = "./"
 model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_filepath,
                                                                monitor='accuracy',
                                                                mode='max',
                                                                save_best_only=True)
-model.fit(image, labels, batch_size=4, shuffle=True,
-          epochs=10, callbacks=model_checkpoint_callback)
-model.predict()
+model.fit(images, labels, batch_size=20
+          , shuffle=True, epochs=20, callbacks=model_checkpoint_callback)
