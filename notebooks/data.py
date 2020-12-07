3e0696fdb6f4622b6b5aaace5d0e38bf0cb2599d
king398 <mithilsalunkhe@outlook.com>
2020-12-07 21:41:56 +0530

    got it working

--- notebooks/data.py
+++ notebooks/data.py
@@ -13,13 +13,13 @@ import matplotlib.pyplot as plt
 
 warnings.filterwarnings("ignore")
 
-samples_df = pd.read_csv(r"F:\Pycharm_projects\Kaggle Cassava\data\train.csv")
+samples_df = pd.read_csv(r"/content/train.csv")
 samples_df = shuffle(samples_df, random_state=42)
 samples_df["label"] = samples_df["label"].astype("str")
 samples_df.head()
 temp_labels = {}
 temp1 = {}
-training_folder = r"F:\Pycharm_projects\Kaggle Cassava\data\train_images"
+training_folder = r"/content/train_images"
 for i in range(len(samples_df)):
 	filename = samples_df.iloc[i, 0]
 	image_label = samples_df.iloc[i, 1]
@@ -41,16 +41,11 @@ for i in tqdm(os.listdir(training_folder)):
 	"""create image list and label
 	"""
 	filenamee = os.path.join(training_folder, i)
-	img = cv2.imread(filenamee)
-
-	imgg = cv2.resize(img, dsize=(300, 300), interpolation=cv2.INTER_CUBIC)
-
 	label = int(temp1.get(filenamee))
+	img = cv2.imread(filenamee)
+	imgg = cv2.resize(img, dsize=(300, 300))
+	imgg = np.asarray(imgg).astype(np.float32)
+	imgg = imgg / 255
 
 	images_list.append(imgg)
 	labels_list.append(label)
-	print(i)
-
-images = np.array(images_list)
-labels = np.array(labels_list)
-print(labels)
--- notebooks/submission.py
+++ notebooks/submission.py
@@ -10,14 +10,11 @@ path = "../input/cassava-leaf-disease-classification/test_images"
 test_file_list = os.listdir(path)
 predictions = []
 for filename in test_file_list:
-	img = cv2.imread(path + "/" + filename)
+	img = tf.keras.preprocessing.image.load_img(path + '/' + filename, target_size=(300, 300))
+	img = tf.keras.preprocessing.image.img_to_array(img=img)
+	img = img / 255.0
 
-	arr = tf.image.random_crop(img, size=(300, 300, 3))
-	arr = arr / 255.0
-	arr = np.array(arr).astype(np.float32)
-	arr = tf.keras.preprocessing.image.img_to_array(arr, dtype=np.float32)
-
-	predictions.append(np.argmax(model.predict(arr)))
+	predictions.append(np.argmax(model.predict(img)))
 
 df = pd.DataFrame(zip(test_file_list, predictions), columns=["image_id", "label"])
 df.to_csv("./submission.csv", index=False)
