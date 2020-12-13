fb53a86abba451db3908f070ad5318bbdd70b608
king398 <mithilsalunkhe@outlook.com>
2020-12-13 12:14:53 +0530

    got it working

---
+++ notebooks/B7Eff.py
@@ -0,0 +1,72 @@
+import numpy as np
+import pandas as pd
+import os
+from tqdm import tqdm
+from sklearn.utils import shuffle
+import warnings
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers
+import datetime
+
+from tensorflow.keras.layers import Dense, Flatten, Dropout, Activation, Conv2D, MaxPool2D, Conv2DTranspose, LeakyReLU, \
+	BatchNormalization
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.mixed_precision import experimental as mixed_precision
+
+policy = mixed_precision.Policy('mixed_float16')
+mixed_precision.set_policy(policy)
+
+model = Sequential()
+VGG = tf.keras.applications.VGG19(input_shape=(300, 300, 3), include_top=False, weights=None)
+Resnet = tf.keras.applications.ResNet152(input_shape=(300, 300, 3), include_top=False, weights=None, classes=5)
+Efficient_net = tf.keras.applications.EfficientNetB7(input_shape=(300, 300, 3), include_top=False)
+
+model.add(Efficient_net)
+model.add(LeakyReLU())
+model.add(BatchNormalization())
+
+model.add(Flatten())
+model.add(LeakyReLU())
+model.add(Dense(512, activation="relu"))
+model.add(LeakyReLU())
+model.add(tf.keras.layers.Activation('relu'))
+model.add(Dense(256, activation="relu"))
+model.add(LeakyReLU())
+model.add(tf.keras.layers.Activation('relu'))
+model.add(LeakyReLU())
+
+model.add(Dense(128, activation="relu"))
+model.add(LeakyReLU())
+
+model.add(Dense(64, activation="relu"))
+
+model.add(LeakyReLU())
+
+model.add(Dense(32, activation="relu"))
+model.add(LeakyReLU())
+
+model.add(Dense(16, activation="relu"))
+
+model.add(LeakyReLU())
+
+model.add(Dense(8, activation="relu"))
+
+model.add(Dense(5, activation="softmax"))
+opt = tf.keras.optimizers.SGD(learning_rate=0.001)
+model.compile(optimizer=opt,
+              loss="sparse_categorical_crossentropy",
+              metrics=['accuracy'])
+checkpoint_filepath = "./"
+log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_filepath,
+                                                               monitor='val_accuracy',
+                                                               mode='max',
+                                                               save_best_only=True)
+
+model.fit(images, labels, batch_size=16
+          , shuffle=True, epochs=15, callbacks=[model_checkpoint_callback, tensorboard_callback], validation_split=0.15)
+model.load(checkpoint_filepath)
+model.save(r"/content/models/modelEfB7.h5", include_optimizer=True)
