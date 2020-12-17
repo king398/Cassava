3a3f435d6796d78835190e5bcd1c6946296dd364
king398 <mithilsalunkhe@outlook.com>
2020-12-17 09:35:55 +0530

    got it working

--- notebooks/data.py
+++ notebooks/data.py
@@ -4,43 +4,44 @@ import os
 from tqdm import tqdm
 from sklearn.utils import shuffle
 import warnings
+import tensorflow as tf
+from PIL import Image
+from random import randrange
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers
+from tensorflow.keras.layers import Dense, Flatten, Dropout, Activation, Conv2D, MaxPool2D, Conv2DTranspose, LeakyReLU, \
+	BatchNormalization
 import cv2
-
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.mixed_precision import experimental as mixed_precision
 
 warnings.filterwarnings("ignore")
-"""Reads the csv file and creates a labels of images .
-"""
+
 samples_df = pd.read_csv(r"/content/train.csv")
 samples_df = shuffle(samples_df, random_state=42)
 samples_df["label"] = samples_df["label"].astype("str")
 samples_df.head()
 temp_labels = {}
-temp1 = {}
 training_folder = r"/content/train_images"
+image = []
+lab = []
+
 for i in range(len(samples_df)):
-	filename = samples_df.iloc[i, 0]
+	image_name = samples_df.iloc[i, 0]
 	image_label = samples_df.iloc[i, 1]
-	la = {filename: image_label}
+	la = {image_name: image_label}
 	temp_labels.update(la)
 print(len(temp_labels))
 for im in tqdm(os.listdir(training_folder)):
-	pr = os.path.join(training_folder, im)
-	labelss = temp_labels.get(im)
-	xc = {pr: labelss}
-	temp1.update(xc)
-
-filenames = np.array([str(temp1.keys())])
-labels = np.array([(str(temp1.values()))
-images_list = []
-labels_list = []
-for i in tqdm(os.listdir(training_folder)):
-
-	filenamee = os.path.join(training_folder, i)
-	label = int(temp1.get(filenamee))
-	img = cv2.imread(filenamee)
+	path = os.path.join(training_folder, im)
+	label = temp_labels.get(im)
+	img = cv2.imread(path)
 	imgg = cv2.resize(img, dsize=(300, 300))
-	imgg = np.asarray(imgg).astype(np.float32)
-	imgg = imgg / 255
+	image.append(imgg)
+	lab.append(label)
 
-	images_list.append(imgg)
-	labels_list.append(label)
+print("ok")
+images = np.array(image)
+labels = np.array(lab).astype(np.float32)
+print(labels)
