098b36f2e9dc3e186636d4b3b73ff1ad7fa1eabb
king398 <mithilsalunkhe@outlook.com>
2020-12-22 18:09:38 +0530

    got it working

--- notebooks/EfficientNetB6.py
+++ notebooks/EfficientNetB6.py
@@ -1,18 +1,9 @@
-import numpy as np
-import pandas as pd
-import os
-from tqdm import tqdm
-from sklearn.utils import shuffle
-import warnings
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras import layers
 import datetime
 
 from tensorflow.keras.layers import Dense, Flatten, Dropout, Activation, Conv2D, MaxPool2D, Conv2DTranspose, LeakyReLU, \
 	BatchNormalization
-from tensorflow import keras
-from tensorflow.keras import layers
 from tensorflow.keras.mixed_precision import experimental as mixed_precision
 
 policy = mixed_precision.Policy('mixed_float16')
@@ -21,29 +12,20 @@ mixed_precision.set_policy(policy)
 model = Sequential()
 
 Efficient_net = tf.keras.applications.EfficientNetB6(input_shape=(300, 300, 3), include_top=False)
-model.add(tf.keras.layers.experimental.preprocessing.PreprocessingLayer(trainable=True))
+model.add(tf.keras.layers.experimental.preprocessing.Rescaling(255))
+model.add(Conv2DTranspose(filters=256, kernel_size=2, strides=1))
 model.add(Efficient_net)
 model.add(LeakyReLU())
 model.add(BatchNormalization())
 model.add(Flatten())
-model.add(LeakyReLU())
 model.add(Dense(512))
-model.add(LeakyReLU())
 model.add(Dense(256))
-model.add(LeakyReLU())
 
 model.add(Dense(128))
-model.add(LeakyReLU())
 
 model.add(Dense(64))
-model.add(LeakyReLU())
-
 model.add(Dense(32))
-model.add(LeakyReLU())
-
 model.add(Dense(16))
-
-model.add(LeakyReLU())
 
 model.add(Dense(8))
 
@@ -60,5 +42,5 @@ model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoi
                                                                mode='max',
                                                                save_best_only=True)
 
-model.fit(images, labels, batch_size=32
+model.fit(images, labels, batch_size=16
           , shuffle=True, epochs=6, callbacks=[model_checkpoint_callback, tensorboard_callback], validation_split=0.2)
