3456ccd47c78a1aeed71a27325e31c9708bf42e3
king398 <mithilsalunkhe@outlook.com>
2020-12-24 20:26:36 +0530

    got it working

--- notebooks/decode image.py
+++ notebooks/decode image.py
@@ -3,8 +3,6 @@ import tensorflow as tf
 
 def decode_image(image):
 	image = tf.image.decode_jpeg(image, channels=3)
-	image = tf.image.random_crop(image, size=(512, 512, 3))
-	image = tf.image.random_brightness(image, 0.2)
-	image = tf.cast(image, tf.float32)
-
+	image = tf.image.resize_images(image, size=(512, 512))
+	image = tf.cast(image, tf.float32) / 255.0
 	return image
