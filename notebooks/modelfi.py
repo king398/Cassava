a1aadedb20c2ba7ce7d6ea47d40d7e6832edc388
king398 <mithilsalunkhe@outlook.com>
2020-12-25 12:01:05 +0530

    got it working

--- notebooks/modelfi.py
+++ notebooks/modelfi.py
@@ -1,29 +1,45 @@
 import tensorflow as tf
+from tensorflow.keras.mixed_precision import experimental as mixed_precision
+
+policy = mixed_precision.Policy('mixed_bfloat16')
+mixed_precision.set_policy(policy)
 
 with strategy.scope():
-	img_adjust_layer = tf.keras.layers.Lambda(tf.keras.applications.resnet50.preprocess_input,
-	                                          input_shape=[*IMAGE_SIZE, 3])
-	base_model = tf.keras.applications.EfficientNetB4(include_top=False)
+	base_model = tf.keras.applications.ResNet152(include_top=False)
 	base_model.trainable = True
 
 	model = tf.keras.Sequential([
-		tf.keras.layers.BatchNormalization(renorm=True),
-		img_adjust_layer,
+		tf.keras.layers.BatchNormalization(),
 		base_model,
+		tf.keras.layers.LeakyReLU(),
 		tf.keras.layers.GlobalAveragePooling2D(),
-		tf.keras.layers.Dense(8, activation='relu'),
-		# tf.keras.layers.BatchNormalization(renorm=True),
+		tf.keras.layers.Dense(256),
+		tf.keras.layers.LeakyReLU(),
+		tf.keras.layers.Dense(128),
+		tf.keras.layers.LeakyReLU(),
+		tf.keras.layers.Dropout(0.15),
+		tf.keras.layers.Dense(64),
+		tf.keras.layers.LeakyReLU(),
+		tf.keras.layers.Dense(32),
+		tf.keras.layers.Dropout(0.15),
+
+		tf.keras.layers.LeakyReLU(),
+		tf.keras.layers.Dense(16),
+
+		tf.keras.layers.LeakyReLU(),
+		tf.keras.layers.Dense(8),
+		tf.keras.layers.LeakyReLU(),
 		tf.keras.layers.Dense(len(CLASSES), activation='softmax')
 	])
 
 	model.compile(
-		optimizer=tf.keras.optimizers.Adam(learning_rate=lr_scheduler, epsilon=0.001),
+		optimizer=tf.keras.optimizers.Adam(lr=0.001, epsilon=0.001),
 		loss='sparse_categorical_crossentropy',
 		metrics=['sparse_categorical_accuracy'])
 
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 
-early = EarlyStopping(monitor='val_accuracy',
+early = EarlyStopping(monitor='val_loss',
                       mode='min',
                       patience=5)
 STEPS_PER_EPOCH = 17118 // BATCH_SIZE
@@ -31,6 +47,7 @@ VALID_STEPS = 4279 // BATCH_SIZE
 
 history = model.fit(train_dataset,
                     steps_per_epoch=STEPS_PER_EPOCH,
-                    epochs=10,
+
+                    epochs=25,
                     validation_data=valid_dataset,
                     validation_steps=VALID_STEPS, callbacks=[early])
