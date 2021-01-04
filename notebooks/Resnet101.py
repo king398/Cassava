a811490f99bb85f89221bbdfee4072d8fc02c61e
king398 <mithilsalunkhe@outlook.com>
2021-01-04 20:35:51 +0530

    ImageNet

--- notebooks/Resnet101.py
+++ notebooks/Resnet101.py
@@ -2,17 +2,12 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.mixed_precision import experimental as mixed_precision
 import pandas as pd
-from tensorflow.keras.layers import Flatten, Dense, LeakyReLU, BatchNormalization, Dropout
+from tensorflow.keras.layers import Flatten, Dense, ReLU, BatchNormalization, Dropout
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
-import datetime
-import os
-import numpy as np
 
 tf.keras.regularizers.l2(l2=0.01)
 policy = mixed_precision.Policy('mixed_float16')
 mixed_precision.set_policy(policy)
-
-
 
 datagen = ImageDataGenerator(validation_split=0.1,
                              dtype=tf.float32, horizontal_flip=True)
@@ -24,49 +19,49 @@ model = tf.keras.Sequential([
 	tf.keras.layers.Input((512, 512, 3)),
 	tf.keras.layers.BatchNormalization(renorm=True),
 	base_model,
-	tf.keras.layers.LeakyReLU(),
+	ReLU(),
 
 	BatchNormalization(),
 
-	tf.keras.layers.Flatten(),
-	tf.keras.layers.Dense(256),
+	Flatten(),
+	Dense(256),
 
-	tf.keras.layers.LeakyReLU(),
+	ReLU(),
 	BatchNormalization(),
 
-	tf.keras.layers.Dense(128),
-	tf.keras.layers.LeakyReLU(),
+	Dense(128),
+	ReLU(),
 	BatchNormalization(),
 
-	tf.keras.layers.Dropout(0.4),
-	tf.keras.layers.LeakyReLU(),
+	Dropout(0.3),
+	ReLU(),
 
 	BatchNormalization(),
 
-	tf.keras.layers.Dense(64),
-	tf.keras.layers.LeakyReLU(),
+	Dense(64),
+	ReLU(),
 	BatchNormalization(),
-	tf.keras.layers.Dense(32),
-	tf.keras.layers.LeakyReLU(),
+	Dense(32),
+	ReLU(),
 	BatchNormalization(),
-	tf.keras.layers.Dropout(0.4),
+	Dropout(0.3),
 
-	tf.keras.layers.LeakyReLU(),
-	BatchNormalization(),
-
-	tf.keras.layers.Dense(16),
-	tf.keras.layers.LeakyReLU(),
-	BatchNormalization(),
-	tf.keras.layers.Dense(8),
-	tf.keras.layers.LeakyReLU(),
+	ReLU(),
 	BatchNormalization(),
 
-	tf.keras.layers.Dense(5, activation='softmax')
+	Dense(16),
+	ReLU(),
+	BatchNormalization(),
+	Dense(8),
+	ReLU(),
+	BatchNormalization(),
+
+	Dense(5, activation='softmax')
 ])
 
 loss = tf.keras.losses.CategoricalCrossentropy(label_smoothing=0.2)
 model.compile(
-	optimizer=tf.keras.optimizers.SGD(0.03),
+	optimizer=tf.keras.optimizers.SGD(0.04),
 	loss='categorical_crossentropy',
 	metrics=['categorical_accuracy'])
 
@@ -74,7 +69,7 @@ early = EarlyStopping(monitor='val_loss',
                       mode='min',
                       patience=5)
 checkpoint_filepath = r"/content/temp/"
-model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+model_checkpoint_callback = ModelCheckpoint(
 	filepath=checkpoint_filepath,
 	save_weights_only=True,
 	monitor='val_categorical_accuracy',
