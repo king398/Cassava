0200c2e08043fa77b04e0d106489c877638f2dd5
king398 <mithilsalunkhe@outlook.com>
2021-01-06 09:18:08 +0530

    ImageNet

--- notebooks/EfficientNetB6.py
+++ notebooks/EfficientNetB6.py
@@ -4,12 +4,22 @@ from tensorflow.keras.mixed_precision import experimental as mixed_precision
 import pandas as pd
 from tensorflow.keras.layers import Flatten, Dense, LeakyReLU, BatchNormalization, Dropout, Input
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow_addons as tfa
 
 policy = mixed_precision.Policy('mixed_float16')
 mixed_precision.set_policy(policy)
 
+
+def augment(image):
+	image = np.array(image).astype(np.float32)
+	image = tf.image.random_brightness(image, 0.4)
+	return image
+
+
 datagen = ImageDataGenerator(validation_split=0.2,
-                             dtype=tf.float32, horizontal_flip=True)
+                             dtype=tf.float32, horizontal_flip=True, preprocessing_function=augment)
 train_csv = pd.read_csv(r"/content/train.csv")
 train_csv["label"] = train_csv["label"].astype(str)
 base_model = tf.keras.applications.EfficientNetB6(include_top=False, weights="imagenet", classes=5)
@@ -28,7 +38,6 @@ model = tf.keras.Sequential([
 	LeakyReLU(),
 	BatchNormalization(trainable=False),
 
-	Dropout(0.4),
 	LeakyReLU(),
 
 	BatchNormalization(trainable=False),
@@ -39,7 +48,6 @@ model = tf.keras.Sequential([
 	Dense(32),
 	LeakyReLU(),
 	BatchNormalization(trainable=False),
-	Dropout(0.4),
 
 	LeakyReLU(),
 	BatchNormalization(trainable=False),
@@ -54,9 +62,16 @@ model = tf.keras.Sequential([
 	Dense(5, activation='softmax')
 ])
 
-loss = tf.keras.losses.CategoricalCrossentropy(label_smoothing=0.2)
+lr_scheduler = tf.keras.optimizers.schedules.ExponentialDecay(
+	initial_learning_rate=1e-3,
+	decay_steps=10000,
+	decay_rate=0.9)
+
+radam = tfa.optimizers.RectifiedAdam(lr=lr_scheduler)
+ranger = tfa.optimizers.Lookahead(radam, sync_period=6, slow_step_size=0.5)
+loss = tf.keras.layers
 model.compile(
-	optimizer=tf.keras.optimizers.SGD(0.04),
+	optimizer=ranger,
 	loss='categorical_crossentropy',
 	metrics=['categorical_accuracy'])
 
@@ -73,13 +88,20 @@ model_checkpoint_callback = ModelCheckpoint(
 history = model.fit(datagen.flow_from_dataframe(dataframe=train_csv,
                                                 directory=r"/content/train_images", x_col="image_id",
                                                 y_col="label", target_size=(512, 512), class_mode="categorical",
-                                                batch_size=8,
+                                                batch_size=12,
                                                 subset="training", shuffle=True),
                     callbacks=[early, model_checkpoint_callback],
                     epochs=10, validation_data=datagen.flow_from_dataframe(dataframe=train_csv,
                                                                            directory=r"/content/train_images",
                                                                            x_col="image_id",
                                                                            y_col="label", target_size=(512, 512),
-                                                                           class_mode="categorical", batch_size=8,
+                                                                           class_mode="categorical", batch_size=12,
                                                                            subset="validation", shuffle=True))
 model.load_weights(checkpoint_filepath)
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'val'], loc='upper left')
+plt.show()
