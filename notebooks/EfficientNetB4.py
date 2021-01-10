06de37556271b2c13a8cc94058b6ff25f4774f20
king398 <mithilsalunkhe@outlook.com>
2021-01-10 08:45:31 +0530

    ImageNet

--- notebooks/EfficientNetB4.py
+++ notebooks/EfficientNetB4.py
@@ -5,15 +5,106 @@ import pandas as pd
 from tensorflow.keras.layers import Flatten, Dense, LeakyReLU, BatchNormalization, Dropout, Input
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 import tensorflow_addons as tfa
-import efficientnet.keras as efn
 import numpy as np
 
 
-def augment(image):
-	image = np.array(image,dtype=np.float16)
-	image = tf.image.random_brightness(image, 0.3)
-	image = tf.image.random_flip_up_down(image)
-	return image
+def log_t(u, t):
+	"""Compute log_t for `u`."""
+	if t == 1.0:
+		return tf.math.log(u)
+	else:
+		return (u ** (1.0 - t) - 1.0) / (1.0 - t)
+
+
+def exp_t(u, t):
+	"""Compute exp_t for `u`."""
+	if t == 1.0:
+		return tf.math.exp(u)
+	else:
+		return tf.math.maximum(0, 1.0 + (1.0 - t) * u) ** (1.0 / (1.0 - t))
+
+
+def compute_normalization_fixed_point(y_pred, t, num_iters=5):
+	"""Returns the normalization value for each example (t > 1.0).
+	  Args:
+	  y_pred: A multi-dimensional tensor with last dimension `num_classes`.
+	  t: Temperature 2 (> 1.0 for tail heaviness).
+	  num_iters: Number of iterations to run the method.
+	  Return: A tensor of same rank as y_pred with the last dimension being 1.
+	"""
+	mu = tf.math.reduce_max(y_pred, -1, keepdims=True)
+	normalized_y_pred_step_0 = y_pred - mu
+	normalized_y_pred = normalized_y_pred_step_0
+	i = 0
+	while i < num_iters:
+		i += 1
+		logt_partition = tf.math.reduce_sum(exp_t(normalized_y_pred, t), -1, keepdims=True)
+		normalized_y_pred = normalized_y_pred_step_0 * (logt_partition ** (1.0 - t))
+
+	logt_partition = tf.math.reduce_sum(exp_t(normalized_y_pred, t), -1, keepdims=True)
+	return -log_t(1.0 / logt_partition, t) + mu
+
+
+def compute_normalization(y_pred, t, num_iters=5):
+	"""Returns the normalization value for each example.
+	  Args:
+	  y_pred: A multi-dimensional tensor with last dimension `num_classes`.
+	  t: Temperature 2 (< 1.0 for finite support, > 1.0 for tail heaviness).
+	  num_iters: Number of iterations to run the method.
+	  Return: A tensor of same rank as activation with the last dimension being 1.
+	"""
+	if t < 1.0:
+		return None  # not implemented as these values do not occur in the authors experiments...
+	else:
+		return compute_normalization_fixed_point(y_pred, t, num_iters)
+
+
+def tempered_softmax(y_pred, t, num_iters=5):
+	"""Tempered softmax function.
+	  Args:
+	  y_pred: A multi-dimensional tensor with last dimension `num_classes`.
+	  t: Temperature tensor > 0.0.
+	  num_iters: Number of iterations to run the method.
+	  Returns:
+	  A probabilities tensor.
+	"""
+	if t == 1.0:
+		normalization_constants = tf.math.log(tf.math.reduce_sum(tf.math.exp(y_pred), -1, keepdims=True))
+	else:
+		normalization_constants = compute_normalization(y_pred, t, num_iters)
+
+	return exp_t(y_pred - normalization_constants, t)
+
+
+def bi_tempered_logistic_loss(y_pred, y_true, t1, t2, num_iters=5):
+	"""Bi-Tempered Logistic Loss with custom gradient.
+	  Args:
+	  y_pred: A multi-dimensional tensor with last dimension `num_classes`.
+	  y_true: A tensor with shape and dtype as y_pred.
+	  t1: Temperature 1 (< 1.0 for boundedness).
+	  t2: Temperature 2 (> 1.0 for tail heaviness, < 1.0 for finite support).
+	  num_iters: Number of iterations to run the method.
+	  Returns:
+	  A loss tensor.
+	"""
+	probabilities = tempered_softmax(y_pred, t2, num_iters)
+
+	temp1 = (log_t(y_true + 1e-10, t1) - log_t(probabilities, t1)) * y_true
+	temp2 = (1 / (2 - t1)) * (tf.math.pow(y_true, 2 - t1) - tf.math.pow(probabilities, 2 - t1))
+	loss_values = temp1 - temp2
+
+	return tf.math.reduce_sum(loss_values, -1)
+
+
+class BiTemperedLogisticLoss(tf.keras.losses.Loss):
+	def __init__(self, t1, t2, n_iter=5):
+		super(BiTemperedLogisticLoss, self).__init__()
+		self.t1 = t1
+		self.t2 = t2
+		self.n_iter = n_iter
+
+	def call(self, y_true, y_pred):
+		return bi_tempered_logistic_loss(y_pred, y_true, self.t1, self.t2, self.n_iter)
 
 
 policy = mixed_precision.Policy('mixed_float16')
@@ -21,10 +112,10 @@ mixed_precision.set_policy(policy)
 
 datagen = ImageDataGenerator(validation_split=0.2,
 
-                             horizontal_flip=True, rescale=1. / 255, preprocessing_function=augment)
+                             horizontal_flip=True, rescale=1. / 255)
 train_csv = pd.read_csv(r"/content/train.csv")
 train_csv["label"] = train_csv["label"].astype(str)
-base_model = tf.keras.applications.EfficientNetB4(weights='imagenet', include_top=False)
+base_model = tf.keras.applications.EfficientNetB4(weights=None, include_top=False)
 base_model.trainable = True
 
 model = tf.keras.Sequential([
@@ -42,18 +133,17 @@ model = tf.keras.Sequential([
 	BatchNormalization(trainable=False),
 
 	LeakyReLU(),
-
 	BatchNormalization(trainable=False),
-
+	Dropout(0.4),
 	Dense(64),
 	LeakyReLU(),
 	BatchNormalization(trainable=False),
 	Dense(32),
 	LeakyReLU(),
 	BatchNormalization(trainable=False),
-
 	LeakyReLU(),
 	BatchNormalization(trainable=False),
+	Dropout(0.4),
 
 	Dense(16),
 	LeakyReLU(),
@@ -65,18 +155,12 @@ model = tf.keras.Sequential([
 	Dense(5, activation='softmax')
 ])
 
-# loss function
-
-
-loss = tf.losses.CategoricalCrossentropy(label_smoothing=0.2)
 model.compile(
-	optimizer=tf.keras.optimizers.SGD(lr=0.03),
-	loss=loss,
+	optimizer=tf.keras.optimizers.SGD(lr=0.02),
+	loss=BiTemperedLogisticLoss(t1=0, t2=1.0),
 	metrics=['categorical_accuracy'])
 
-early = EarlyStopping(monitor='val_loss',
-                      mode='min',
-                      patience=5)
+early = EarlyStopping(monitor='val_loss', mode='min', patience=5)
 checkpoint_filepath = r"/content/temp/"
 model_checkpoint_callback = ModelCheckpoint(
 	filepath=checkpoint_filepath,
@@ -87,13 +171,13 @@ model_checkpoint_callback = ModelCheckpoint(
 history = model.fit(datagen.flow_from_dataframe(dataframe=train_csv,
                                                 directory=r"/content/train_images", x_col="image_id",
                                                 y_col="label", target_size=(512, 512),
-                                                batch_size=12,
+                                                batch_size=16,
                                                 subset="training", shuffle=True),
                     callbacks=[early, model_checkpoint_callback],
                     epochs=10, validation_data=datagen.flow_from_dataframe(dataframe=train_csv,
                                                                            directory=r"/content/train_images",
                                                                            x_col="image_id",
                                                                            y_col="label", target_size=(512, 512),
-                                                                           batch_size=12,
+                                                                           batch_size=16,
                                                                            subset="validation", shuffle=True))
 model.load_weights(checkpoint_filepath)
--- notebooks/Efficientnetb5.py
+++ notebooks/Efficientnetb5.py
@@ -9,8 +9,7 @@ import numpy as np
 
 
 def augment(image):
-	image = np.array(image,dtype=np.float32)
-	image = tf.image.random_brightness(image, 0.3)
+	image = np.array(image, dtype=np.float32)
 	image = tf.image.random_flip_up_down(image)
 	return image
 
@@ -20,7 +19,7 @@ mixed_precision.set_policy(policy)
 
 datagen = ImageDataGenerator(validation_split=0.2,
 
-                             horizontal_flip=True, rescale=1. / 255, preprocessing_function=augment)
+                             horizontal_flip=True, rescale=1. / 255, preprocessing_function=augment())
 train_csv = pd.read_csv(r"/content/train.csv")
 train_csv["label"] = train_csv["label"].astype(str)
 base_model = tf.keras.applications.EfficientNetB5(weights='imagenet', include_top=False)
@@ -41,7 +40,7 @@ model = tf.keras.Sequential([
 	BatchNormalization(trainable=False),
 
 	LeakyReLU(),
-
+	Dropout(0.4),
 	BatchNormalization(trainable=False),
 
 	Dense(64),
@@ -50,7 +49,7 @@ model = tf.keras.Sequential([
 	Dense(32),
 	LeakyReLU(),
 	BatchNormalization(trainable=False),
-
+	Dropout(0.4),
 	LeakyReLU(),
 	BatchNormalization(trainable=False),
 
@@ -64,13 +63,25 @@ model = tf.keras.Sequential([
 	Dense(5, activation='softmax')
 ])
 
+
 # loss function
+class GeneralizedCrossEntropy(tf.losses.Loss):
+	def __init__(self, eta=0.7):
+		'''
+		Paper: https://arxiv.org/abs/1805.07836
+		'''
+		super(GeneralizedCrossEntropy, self).__init__()
+		self.eta = eta
+
+	def call(self, y_true, y_pred):
+		t_loss = (1 - tf.pow(tf.reduce_sum(y_true * y_pred, axis=-1),
+		                     self.eta)) / self.eta
+		return tf.reduce_mean(t_loss)
 
 
-loss = tf.losses.CategoricalCrossentropy()
 model.compile(
 	optimizer=tf.keras.optimizers.SGD(lr=0.03),
-	loss=loss,
+	loss=GeneralizedCrossEntropy(),
 	metrics=['categorical_accuracy'])
 
 early = EarlyStopping(monitor='val_loss',
