107bf9db49b4d15e47f78aab228d2bd33dfa8671
king398 <mithilsalunkhe@outlook.com>
2021-01-18 18:02:12 +0530

    88.40 acc

--- notebooks/Resnet101.py
+++ notebooks/Resnet101.py
@@ -5,6 +5,7 @@ import pandas as pd
 from tensorflow.keras.layers import Flatten, Dense, LeakyReLU, BatchNormalization, Dropout
 import keras.backend as K
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+import efficientnet.keras as efn
 import tensorflow_addons as tfa
 
 policy = mixed_precision.Policy('mixed_float16')
@@ -14,60 +15,19 @@ datagen = ImageDataGenerator(rescale=1. / 255, validation_split=0.2, horizontal_
 train_csv = pd.read_csv(r"/content/train.csv")
 train_csv["label"] = train_csv["label"].astype(str)
 
-
-def categorical_focal_loss_with_label_smoothing(gamma=2.0, alpha=0.25, ls=0.1, classes=5.0):
-	"""
-	Implementation of Focal Loss from the paper in multiclass classification
-	Formula:
-		loss = -alpha*((1-p)^gamma)*log(p)
-		y_ls = (1 - α) * y_hot + α / classes
-	Parameters:
-		alpha -- the same as wighting factor in balanced cross entropy
-		gamma -- focusing parameter for modulating factor (1-p)
-		ls    -- label smoothing parameter(alpha)
-		classes     -- No. of classes
-	Default value:
-		gamma -- 2.0 as mentioned in the paper
-		alpha -- 0.25 as mentioned in the paper
-		ls    -- 0.1
-		classes     -- 4
-	"""
-
-	def focal_loss(y_true, y_pred):
-		# Define epsilon so that the backpropagation will not result in NaN
-		# for 0 divisor case
-		epsilon = K.epsilon()
-		# Add the epsilon to prediction value
-		# y_pred = y_pred + epsilon
-		# label smoothing
-		y_pred_ls = (1 - ls) * y_pred + ls / classes
-		# Clip the prediction value
-		y_pred_ls = K.clip(y_pred_ls, epsilon, 1.0 - epsilon)
-		# Calculate cross entropy
-		cross_entropy = -y_true * K.log(y_pred_ls)
-		# Calculate weight that consists of  modulating factor and weighting factor
-		weight = alpha * y_true * K.pow((1 - y_pred_ls), gamma)
-		# Calculate focal loss
-		loss = weight * cross_entropy
-		# Sum the losses in mini_batch
-		loss = K.sum(loss, axis=1)
-		return loss
-
-	return focal_loss
-
-
-base_model = tf.keras.applications.ResNet101(weights="imagenet")
+base_model = tf.keras.applications.ResNet50(weights='imagenet', input_shape=(512, 512, 3), include_top=True)
 
 base_model.trainable = True
 
 model = tf.keras.Sequential([
+
 	tf.keras.layers.Input((512, 512, 3)),
 	tf.keras.layers.BatchNormalization(renorm=True),
 	base_model,
 	BatchNormalization(),
 	tf.keras.layers.LeakyReLU(),
 	tf.keras.layers.Flatten(),
-  tf.keras.layers.Dense(512),
+	tf.keras.layers.Dense(512),
 	BatchNormalization(),
 
 	tf.keras.layers.LeakyReLU(),
@@ -99,9 +59,12 @@ model = tf.keras.Sequential([
 	tf.keras.layers.LeakyReLU(),
 	tf.keras.layers.Dense(8),
 	tf.keras.layers.LeakyReLU(),
-	tf.keras.layers.Dense(5, activation='softmax')
+	tf.keras.layers.Dense(5, activation='softmax', dtype='float32')
 ])
-opt = tf.keras.optimizers.SGD(0.03)
+first_decay_steps = 1000
+
+lr = (tf.keras.experimental.CosineDecayRestarts(0.04, first_decay_steps))
+opt = tf.keras.optimizers.SGD(lr)
 model.compile(
 	optimizer=opt,
 	loss=tf.keras.losses.CategoricalCrossentropy(),
@@ -117,13 +80,13 @@ model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
 history = model.fit(datagen.flow_from_dataframe(dataframe=train_csv,
                                                 directory=r"/content/train_images", x_col="image_id",
                                                 y_col="label", target_size=(512, 512), class_mode="categorical",
-                                                batch_size=16,
+                                                batch_size=24,
                                                 subset="training", shuffle=True),
                     callbacks=[model_checkpoint_callback],
-                    epochs=25, validation_data=datagen.flow_from_dataframe(dataframe=train_csv,
+                    epochs=20, validation_data=datagen.flow_from_dataframe(dataframe=train_csv,
                                                                            directory=r"/content/train_images",
                                                                            x_col="image_id",
                                                                            y_col="label", target_size=(512, 512),
-                                                                           class_mode="categorical", batch_size=16,
+                                                                           class_mode="categorical", batch_size=24,
                                                                            subset="validation", shuffle=True))
 model.load_weights(checkpoint_filepath)
--- notebooks/submission.py
+++ notebooks/submission.py
@@ -8,26 +8,27 @@ model1 = tf.keras.models.load_model(r"../input/models-gcs/88effnetb3moredata", c
 model2 = tf.keras.models.load_model(r"../input/models-gcs/88effnetb3custom", compile=False)
 model3 = tf.keras.models.load_model(r"../input/models-gcs/88effnetb3noisyincludetopTrue", compile=False)
 
-
-
-
-
-
-path = "../input/cassava-leaf-disease-classification/test_images"
+path = "../input/cassava-leaf-disease-classification/train_images"
 test_file_list = os.listdir(path)
 predictions = []
+tta_pred = []
 model1_predict_list = []
+tta = 3
 for filename in tqdm(test_file_list):
-	img = tf.keras.preprocessing.image.load_img(path + "/" + filename, target_size=(512, 512))
-	arr = tf.keras.preprocessing.image.img_to_array(img)
-	arr = tf.image.random_flip_left_right(arr)
-	arr = tf.expand_dims(arr / 255., 0)
-	model1_predict = np.argmax(model1.predict(arr))
-	model2_predict = np.argmax(model2.predict(arr))
-	model3_predict = np.argmax(model3.predict(arr))
+	tta_pred = []
 
-	pre = [model1_predict, model2_predict, model3_predict]
-	predictions.append(int(max(set(pre), key=pre.count)))
+	for i in range(tta):
+		img = tf.keras.preprocessing.image.load_img(path + "/" + filename, target_size=(512, 512))
+		arr = np.array(img, dtype=np.float32)
+		arr = tf.image.random_flip_left_right(arr)
+		arr = tf.expand_dims(arr / 255., 0)
+		model1_predict = np.argmax(model1.predict(arr, use_multiprocessing=True))
+		model2_predict = np.argmax(model2.predict(arr, use_multiprocessing=True))
+		model3_predict = np.argmax(model3.predict(arr, use_multiprocessing=True))
+		pre = [model1_predict, model2_predict, model3_predict]
+		tta_pred.append(int(max(set(pre), key=pre.count)))
+
+	predictions.append(max(set(tta_pred), key=pre.count))
 
 df = pd.DataFrame(zip(test_file_list, predictions), columns=["image_id", "label"])
 df.to_csv("./submission.csv", index=False)
