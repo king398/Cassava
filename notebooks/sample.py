00d96f6caaf5aed7fcba568e7345d76684c200a2
king398 <mithilsalunkhe@outlook.com>
2021-01-28 11:53:57 +0530

    d

--- notebooks/sample.py
+++ notebooks/sample.py
@@ -1,85 +1,28 @@
 import tensorflow as tf
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.mixed_precision import experimental as mixed_precision
-import pandas as pd
-from tensorflow.keras.layers import Flatten, Dense, LeakyReLU, BatchNormalization, Dropout, Input
-from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
-import tensorflow_addons as tfa
+from vit_keras import vit
+from keras.datasets import mnist
 
+policy = mixed_precision.Policy('mixed_float16')
+mixed_precision.set_policy(policy)
 
-datagen = ImageDataGenerator(validation_split=0.2,
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-                              horizontal_flip=True, rescale=1. / 255)
-train_csv = pd.read_csv(r"/content/train.csv")
-train_csv["label"] = train_csv["label"].astype(str)
-base_model = tf.keras.applications.EfficientNetB5(include_top=False, weights="imagenet", classes=5)
-base_model.trainable = True
-
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
+base_model = vit.vit_l32(
+activation=None,
+pretrained=False,
+include_top=False,
+pretrained_top=False,
+)
 model = tf.keras.Sequential([
-    Input((512, 512, 3)),
-    BatchNormalization(renorm=True, trainable=False),
-    base_model,
-    Flatten(),
-    Dense(256),
-
-    LeakyReLU(),
-    BatchNormalization(trainable=False),
-
-    Dense(128),
-    LeakyReLU(),
-    BatchNormalization(trainable=False),
-
-    LeakyReLU(),
-
-    BatchNormalization(trainable=False),
-
-    Dense(64),
-    LeakyReLU(),
-    BatchNormalization(trainable=False),
-    Dense(32),
-    LeakyReLU(),
-    BatchNormalization(trainable=False),
-
-    LeakyReLU(),
-    BatchNormalization(trainable=False),
-
-    Dense(16),
-    LeakyReLU(),
-    BatchNormalization(trainable=False),
-    Dense(8),
-    LeakyReLU(),
-    BatchNormalization(trainable=False),
-
-    Dense(5, activation='softmax')
+	base_model,
+	tf.keras.layers.Flatten(),
+	tf.keras.layers.Dense(5, activation='softmax', dtype='float32')
 ])
-radam = tfa.optimizers.RectifiedAdam()
-ranger = tfa.optimizers.Lookahead(radam, sync_period=6, slow_step_size=0.5)
-loss = tf.keras.losses.CategoricalCrossentropy(label_smoothing=0.1)
 model.compile(
-    optimizer=ranger,
-    loss=loss,
-    metrics=['categorical_accuracy'])
-
-early = EarlyStopping(monitor='val_loss',
-                      mode='min',
-                      patience=5)
-checkpoint_filepath = r"/content/temp/"
-model_checkpoint_callback = ModelCheckpoint(
-    filepath=checkpoint_filepath,
-    save_weights_only=True,
-    monitor='val_categorical_accuracy',
-    mode='max',
-    save_best_only=True)
-history = model.fit(datagen.flow_from_dataframe(dataframe=train_csv,
-                                                directory=r"/content/train_images", x_col="image_id",
-                                                y_col="label", target_size=(512, 512),
-                                                batch_size=6,
-                                                subset="training", shuffle=True),
-                    callbacks=[early, model_checkpoint_callback],
-                    epochs=10, validation_data=datagen.flow_from_dataframe(dataframe=train_csv,
-                                                                           directory=r"/content/train_images",
-                                                                           x_col="image_id",
-                                                                           y_col="label", target_size=(512, 512),
-                                                                           batch_size=6,
-                                                                           subset="validation", shuffle=True))
-model.load_weights(checkpoint_filepath)
+optimizer="adam",
+loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+metrics=['categorical_accuracy'])
+model.fit(x=x_train, y=y_train, batch_size=32, epochs=20, shuffle=True, validation_data=(x_test, y_test))̥
